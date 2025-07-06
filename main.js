3cd4cce8c0e1365cc0f221dcc92829466ab9275c
Yusuf Ahmed <yusufahmed0625@gmail.com>
2025-07-06 17:49:32 +0100

    added basic styling

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ clockDate = document.getElementById('clock-date');
 
 const updateTime = () => {
     const today = new Date()
-    console.log(today)
+    // console.log(today)
     clockTime.textContent = `${String(today.getHours()).padStart(2, "0")}:${String(today.getMinutes()).padStart(2, "0")}:${String(today.getSeconds()).padStart(2, "0")}`;
     clockDate.textContent = `${String(today.getDate()).padStart(2, "0")}/${String(today.getMonth() + 1).padStart(2, "0")}/${today.getFullYear()}`
 }
